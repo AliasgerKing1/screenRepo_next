db33a8223476b8f2137063afb88a23217102c125
alihuede <76582089+alihuede@users.noreply.github.com>
2023-04-27 11:44:58 +0530

    changes

--- screenrepo/src/components/shared/Company/Company.js
+++ screenrepo/src/components/shared/Company/Company.js
@@ -1,54 +1,10 @@
 import React, { useState, useEffect } from "react";
-import Spinner from "../Spinner/Spinner";
-import ImageGallery from "../ImageGallery/ImageGallery";
 
 const Company = () => {
-  const [card, setCard] = useState([]);
-  const [page, setPage] = useState(9);
-  const [loading, setLoading] = useState(true);
-
-  const getCardData = async () => {
-    const res = await fetch(
-      `http://localhost:4000/api/screenShot/scroll?_limit=9&_page=${page}`
-    );
-    const data = await res.json();
-    setCard((prev) => [...prev, ...data]);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getCardData();
-  }, [page]);
-
-  const handelInfiniteScroll = async () => {
-    // console.log("scrollHeight" + document.documentElement.scrollHeight);
-    // console.log("innerHeight" + window.innerHeight);
-    // console.log("scrollTop" + document.documentElement.scrollTop);
-    try {
-      if (
-        window.innerHeight + document.documentElement.scrollTop + 1 >=
-        document.documentElement.scrollHeight
-      ) {
-        setLoading(true);
-        setPage((prev) =>
-          prev + 9
-          );
-        console.log(page)
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handelInfiniteScroll);
-    return () => window.removeEventListener("scroll", handelInfiniteScroll);
-  }, []);
 
   return (
     <>
-      <ImageGallery screenInfo={card} />
-      {loading && <Spinner />}
+      
     </>
   );
 };
--- screenrepo/src/components/shared/Searchbar/Searchbar.js
+++ screenrepo/src/components/shared/Searchbar/Searchbar.js
@@ -7,18 +7,18 @@ const SearchBar = () => {
   <div className="row">
     <div className="col-md-6 offset-md-3">
       <div className="search-area">
-      <img src="/assets/images/search.svg" alt="search logo" style={{width : "40px"}} />
+      <img src="/assets/images/search.svg" className='mt-1' alt="search logo" style={{width : "30px"}} />
         {/* <img
           src="/assets/images/search.svg"
           alt="search logo"
           style={{top : '20px', position : "absolute", left: '13%', width: "40px"}}
-        />
+        /> */}
         <input
           type="text"
-          className="btn2"
+          className="btn2 mt-2"
           style={{width: '379px'}}
           placeholder=" | Search screen by name, category and app name"
-        /> */}
+        />
         {/* <div className="suggestion-box" *ngIf="showSuggestions">
           <ul>
             <ng-container *ngFor="let suggestion of suggestions">
